03fa8a57bd21f59ca3479a17a62f9d1a8dd1fe23
seljuketurks <102949261+seljuketurks@users.noreply.github.com>
2022-04-08 15:48:24 +0800

    Update index.php

--- project/index.php
+++ project/index.php
@@ -11,32 +11,32 @@
 <body>
 
 <div class="jumbotron text-center">
-  <h1>Welcome to DepEd</h1>
-  <p>This is a simple demo on how to deploy web application to heroku!</p> 
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
 </div>
   
 <div class="container">
   <div class="row">
     <div class="col-sm-4">
-      <h3>GALLERY 1</h3>
+      <h3>Column 1</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
     <div class="col-sm-4">
-      <h3>GALLERY 2</h3>
-      <p>
-       </p>
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
     <div class="col-sm-4">
-      <h3>GALLERY 3</h3>        
+      <h3>Column 3</h3>        
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
   </div>
 </div>
-
+<?php
+$txt = "PHP";
+echo "I love $txt!";
+?>
 </body>
 </html>
-
-
