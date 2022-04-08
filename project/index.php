50396a97c1195764ba513f9a168006d6dc202383
seljuketurks <102949261+seljuketurks@users.noreply.github.com>
2022-04-08 15:49:47 +0800

    Update index.php

--- project/index.php
+++ project/index.php
@@ -11,8 +11,8 @@
 <body>
 
 <div class="jumbotron text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p> 
+  <h1>Welcome to My Website</h1>
+  <p>This is a simple demo project</p> 
 </div>
   
 <div class="container">
