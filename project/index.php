c07e21c1497241551cbb82e79e6a7af707c5c1ee
seljuketurks <102949261+seljuketurks@users.noreply.github.com>
2022-04-08 15:27:12 +0800

    Update index.php

--- project/index.php
+++ project/index.php
@@ -1,4 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to Hello World</title>
+</head>
+<body>
 <?php
 $txt = "PHP";
 echo "I love $txt!";
 ?>
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
